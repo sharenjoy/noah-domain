1dbccbb42ef3cce01fe83d14ce948e9b5590bdef
Ronald <ronald.jian@gmail.com>
2026-03-27 01:08:59 +0800

    remove pinying

--- src/Models/Cms/Tag.php
+++ src/Models/Cms/Tag.php
@@ -2,13 +2,11 @@
 
 namespace Sharenjoy\NoahDomain\Models\Cms;
 
-use ArrayAccess;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection as DbCollection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Collection;
-use Overtrue\Pinyin\Pinyin;
 use RalphJSmit\Laravel\SEO\Support\HasSEO;
 use RalphJSmit\Laravel\SEO\Support\SEOData;
 use Sharenjoy\NoahDomain\Models\Cms\Post;
@@ -89,22 +87,6 @@ class Tag extends Model implements Sortable
         return $query->whereRaw('lower(' . $this->getQuery()->getGrammar()->wrap('name->' . $locale) . ') like ?', ['%' . mb_strtolower($name) . '%']);
     }
 
-    public static function findOrCreate(
-        string | array | ArrayAccess $values,
-        string | null $type = null,
-        string | null $locale = null,
-    ): Collection | Tag | static {
-        $tags = collect($values)->map(function ($value) use ($type, $locale) {
-            if ($value instanceof self) {
-                return $value;
-            }
-
-            return static::findOrCreateFromString($value, $type, $locale);
-        });
-
-        return is_string($values) ? $tags->first() : $tags;
-    }
-
     public static function getWithType(string $type): DbCollection
     {
         return static::withType($type)->get();
@@ -129,23 +111,6 @@ class Tag extends Model implements Sortable
         return static::query()
             ->where("name->{$locale}", $name)
             ->get();
-    }
-
-    public static function findOrCreateFromString(string $name, ?string $type = null, ?string $locale = null)
-    {
-        $locale = $locale ?? static::getLocale();
-
-        $tag = static::findFromString($name, $type, $locale);
-
-        if (! $tag) {
-            $tag = static::create([
-                'name' => [$locale => $name],
-                'slug' => str()->slug(Pinyin::convert($name)),
-                'type' => $type,
-            ]);
-        }
-
-        return $tag;
     }
 
     public static function getTypes(): Collection
