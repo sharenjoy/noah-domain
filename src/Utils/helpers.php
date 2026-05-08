c3b85a22e52c1c769d7f53df1976b0bded03f16b
Ronald(MacMini) <ronald.jian@gmail.com>
2026-05-08 10:39:29 +0800

    add tree getter

--- src/Utils/helpers.php
+++ src/Utils/helpers.php
@@ -23,3 +23,55 @@ if (!function_exists('media_url')) {
         return \Sharenjoy\NoahDomain\Utils\Media::imgUrl($ids);
     }
 }
+
+if (!function_exists('category_tree')) {
+    function category_tree($ids): mixed
+    {
+        $categories = \Sharenjoy\NoahDomain\Models\Cms\Category::query()
+            ->where('is_active', true)
+            ->select('id', 'title', 'slug', 'parent_id')
+            ->orderBy('order')
+            ->get();
+
+        $grouped = $categories->groupBy('parent_id');
+
+        $buildTree = function ($parentId) use (&$buildTree, $grouped) {
+            return $grouped->get($parentId, collect())
+                ->map(fn ($category) => [
+                    'id' => $category->id,
+                    'title' => $category->title,
+                    'slug' => $category->slug,
+                    'children' => $buildTree($category->id),
+                ])
+                ->values();
+        };
+
+        return $buildTree(-1);
+    }
+}
+
+if (!function_exists('menu_tree')) {
+    function menu_tree($ids): mixed
+    {
+        $menus = \Sharenjoy\NoahDomain\Models\Cms\Menu::query()
+            ->where('is_active', true)
+            ->select('id', 'title', 'slug', 'parent_id')
+            ->orderBy('order')
+            ->get();
+
+        $grouped = $menus->groupBy('parent_id');
+
+        $buildTree = function ($parentId) use (&$buildTree, $grouped) {
+            return $grouped->get($parentId, collect())
+                ->map(fn ($menu) => [
+                    'id' => $menu->id,
+                    'title' => $menu->title,
+                    'slug' => $menu->slug,
+                    'children' => $buildTree($menu->id),
+                ])
+                ->values();
+        };
+
+        return $buildTree(-1);
+    }
+}
